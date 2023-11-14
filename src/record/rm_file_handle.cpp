96b4eb64f8fa82b530e39bc94c8c4fde651c3dfa
lemnsoda <2904883570@qq.com>
2023-11-14 16:59:16 +0800

    lab1

---
+++ src/record/rm_file_handle.cpp
@@ -0,0 +1,223 @@
+#include "rm_file_handle.h"
+
+/**
+ * @description: 获取当前表中记录号为rid的记录
+ * @param {Rid&} rid 记录号，指定记录的位置
+ * @param {Context*} context
+ * @return {unique_ptr<RmRecord>} rid对应的记录对象指针
+ */
+std::unique_ptr<RmRecord> RmFileHandle::get_record(const Rid& rid, Context* context) const {
+    // Todo:
+    // 1. 获取指定记录所在的page handle
+    // 2. 初始化一个指向RmRecord的指针（赋值其内部的data和size）
+	
+	//获取记录所在的页号
+    RmPageHandle pageHandle = fetch_page_handle(rid.page_no);
+    
+    
+    //获取到record的指针并且返回
+    int size = file_hdr_.record_size;
+    char* data = pageHandle.get_slot(rid.slot_no);
+
+    std::unique_ptr<RmRecord> pRecord(new RmRecord(size, data));
+
+    return pRecord;
+}
+
+/**
+ * @description: 在当前表中插入一条记录，不指定插入位置
+ * @param {char*} buf 要插入的记录的数据
+ * @param {Context*} context
+ * @return {Rid} 插入的记录的记录号（位置）
+ */
+Rid RmFileHandle::insert_record(char* buf, Context* context) {
+    // Todo:
+    // 1. 获取当前未满的page handle
+    // 2. 在page handle中找到空闲slot位置
+    // 3. 将buf复制到空闲slot位置
+    // 4. 更新page_handle.page_hdr中的数据结构
+    // 注意考虑插入一条记录后页面已满的情况，需要更新file_hdr_.first_free_page_no
+
+	// 获取空闲页
+    RmPageHandle pageHandle = create_page_handle();
+    int pageNo = pageHandle.page->GetPageId().page_no;
+    
+    int bitmapSize = pageHandle.file_hdr->bitmap_size;
+        
+        // get the j-th bit of the i-th byte
+    #define bit(byteN, bitN)	\
+        ((pageHandle.bitmap[byteN] >> (7 - bitN)) & 1)
+        // 定位到空的slot
+    #define freeSlot(byteN, bitN)	\
+        (byteN * BITMAP_WIDTH + bitN) * recordSize + pageHandle.slots
+        //将Buf复制到slot处
+    #define setBufToSlot(buf, pageHandle)	\
+        int recordSize = pageHandle.file_hdr->record_size;	\
+        memcpy(freeSlot(byteN, bitN), buf, recordSize);
+        //页面是否为空
+    #define isPageFull()	\
+        (pageHandle.page_hdr->num_records == bitmapSize * 8)
+        // 获取到第N位bit
+    #define bitNbit		\
+        (1 << (7 - bitN))
+        
+	
+    for (int byteN = 0; byteN < bitmapSize; byteN++) // tranverse every byte
+    	for (int bitN = 0; bitN < 8; bitN++) // tranverse every bit
+    		if (!bit(byteN, bitN)) { // found a free slot
+    		
+    			// 将Buf放到空的slot处
+    			setBufToSlot(buf, pageHandle)
+    			
+    			// 更新文件头
+    			++pageHandle.page_hdr->num_records;
+    			// set the first false found to true
+    			pageHandle.bitmap[byteN] |= bitNbit;
+    			
+    			if (isPageFull()) {
+    				//在插入元组后页是否已满
+    				RmPageHandle newPageHandle = create_page_handle();
+    				release_page_handle(newPageHandle);
+    			}
+    			return Rid{pageNo, 8 * byteN + bitN};
+    		}
+    		
+    // not expect to enter here
+    return Rid{-1, -1};
+}
+
+/**
+ * @description: 删除记录文件中记录号为rid的记录
+ * @param {Rid&} rid 要删除的记录的记录号（位置）
+ * @param {Context*} context
+ */
+ 
+#define fetchPageHandlePlus(rid)	\
+	RmPageHandle pageHandle = 		\
+	fetch_page_handle(rid.page_no);	\
+	int byteN = rid.slot_no / 8;	\
+	int bitN = rid.slot_no - byteN;
+	
+void RmFileHandle::delete_record(const Rid& rid, Context* context) {
+    // Todo:
+    // 1. 获取指定记录所在的page handle
+    // 2. 更新page_handle.page_hdr中的数据结构
+    // 注意考虑删除一条记录后页面未满的情况，需要调用release_page_handle()
+
+	// 获取到指定记录所在的page handle
+    fetchPageHandlePlus(rid);
+
+	// set bitN-th bit to false
+    pageHandle.bitmap[byteN] &= ~bitNbit;
+    --pageHandle.page_hdr->num_records;
+    
+    int bitmapSize = pageHandle.file_hdr->bitmap_size;
+
+    if (isPageFull())
+        release_page_handle(pageHandle);
+}
+
+/**
+ * @description: 更新记录文件中记录号为rid的记录
+ * @param {Rid&} rid 要更新的记录的记录号（位置）
+ * @param {char*} buf 新记录的数据
+ * @param {Context*} context
+ */
+void RmFileHandle::update_record(const Rid& rid, char* buf, Context* context) {
+    // Todo:
+    // 1. 获取指定记录所在的page handle
+    // 2. 更新记录
+
+    fetchPageHandlePlus(rid);
+	
+	// 将指定记录放到rid中
+	setBufToSlot(buf, pageHandle)
+}
+
+/**
+ * 以下函数为辅助函数，仅提供参考，可以选择完成如下函数，也可以删除如下函数，在单元测试中不涉及如下函数接口的直接调用
+ */
+/**
+ * @description: 获取指定页面的页面句柄
+ * @param {int} page_no 页面号
+ * @return {RmPageHandle} 指定页面的句柄
+ */
+RmPageHandle RmFileHandle::fetch_page_handle(int page_no) const {
+    // Todo:
+    // 使用缓冲池获取指定页面，并生成page_handle返回给上层
+    // if page_no is invalid, throw PageNotExistError exception
+
+	// if page_no is invalid, throw PageNotExistError exception
+    if (page_no == INVALID_PAGE_ID) throw PageNotExistError("page_no Invalid", page_no);
+    
+	PageId pageId = {fd_, page_no};
+	// use buffer pool to fetch the specified page
+    Page* page = buffer_pool_manager_->FetchPage(pageId);
+	// attach page and file handle
+    RmPageHandle pageHandle(&file_hdr_, page);
+
+    return pageHandle;
+}
+
+/**
+ * @description: 创建一个新的page handle
+ * @return {RmPageHandle} 新的PageHandle
+ */
+RmPageHandle RmFileHandle::create_new_page_handle() {
+    // Todo:
+    // 1.使用缓冲池来创建一个新page
+    // 2.更新page handle中的相关信息
+    // 3.更新file_hdr_
+
+	// create a new page
+    PageId pageId = {fd_, INVALID_PAGE_ID};
+    Page* page = buffer_pool_manager_->NewPage(&pageId);
+
+	// update file handle
+    ++file_hdr_.num_pages;
+	// attach page and file handle
+    RmPageHandle page_handle(&file_hdr_, page);
+
+    return page_handle;
+}
+
+/**
+ * @brief 创建或获取一个空闲的page handle
+ *
+ * @return RmPageHandle 返回生成的空闲page handle
+ * @note pin the page, remember to unpin it outside!
+ */
+RmPageHandle RmFileHandle::create_page_handle() {
+    // Todo:
+    // 1. 判断file_hdr_中是否还有空闲页
+    //     1.1 没有空闲页：使用缓冲池来创建一个新page；可直接调用create_new_page_handle()
+    //     1.2 有空闲页：直接获取第一个空闲页
+    // 2. 生成page handle并返回给上层
+    
+    int freeNo = file_hdr_.first_free_page_no;
+
+    if (freeNo < 0){ // no free page
+        return create_new_page_handle();
+    }
+	// fetch the first free page
+	RmPageHandle pageHandle = fetch_page_handle(freeNo);
+	// return to upper layer
+    return pageHandle;
+}
+
+/**
+ * @description: 当一个页面从没有空闲空间的状态变为有空闲空间状态时，更新文件头和页头中空闲页面相关的元数据
+ */
+void RmFileHandle::release_page_handle(RmPageHandle& page_handle) {
+    // Todo:
+    // 当page从已满变成未满，考虑如何更新：
+    // 1. page_handle.page_hdr->next_free_page_no
+    // 2. file_hdr_.first_free_page_no
+	
+	// page not full, next free page points at self
+	// get page no
+	int pageNo = page_handle.page->GetPageId().page_no;
+	
+    page_handle.page_hdr->next_free_page_no = pageNo;
+    file_hdr_.first_free_page_no = pageNo;
+}
---
+++ src/record/rm_scan.cpp
@@ -0,0 +1,69 @@
+#include "rm_scan.h"
+
+#include "rm_file_handle.h"
+
+/**
+ * @brief 初始化file_handle和rid
+ *
+ * @param file_handle
+ */
+RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
+    // Todo:
+    // 初始化file_handle和rid（指向第一个存放了记录的位置）
+    //这是查有record的位置，而不是查空闲的表，不要弄错了!
+    int maxpage = file_handle_->file_hdr_.num_pages;
+    int pageno = 1;
+    if(maxpage > 1){//首页是文件头
+        for(pageno = 1; pageno <  maxpage; pageno++){
+            if(file_handle_->fetch_page_handle(pageno).page_hdr->num_records > 0){ 
+                int i = Bitmap::first_bit(1, file_handle_->fetch_page_handle(pageno).bitmap, file_handle_->file_hdr_.num_records_per_page);
+                rid_.page_no = pageno; 
+                rid_.slot_no = i;
+                return;
+            }
+        }
+    }
+    rid_=Rid{-1,-1};
+}
+
+/**
+ * @brief 找到文件中下一个存放了记录的位置
+ */
+void RmScan::next() {
+    // Todo:
+    // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
+    int maxpage = file_handle_->file_hdr_.num_pages;
+    int pageno = rid_.page_no;
+    int slotno = rid_.slot_no;
+    for(;pageno < maxpage; pageno++){
+        int i = Bitmap::next_bit(1, file_handle_->fetch_page_handle(pageno).bitmap, file_handle_->file_hdr_.num_records_per_page, slotno);
+        if(i == file_handle_->file_hdr_.num_records_per_page){   
+            slotno = -1;
+            continue;  
+        }
+        else{   
+            rid_.page_no = pageno;
+            rid_.slot_no = i;
+            return;
+        }
+    }
+    rid_=Rid{-1,-1};
+}
+
+/**
+ * @brief ​ 判断是否到达文件末尾
+ */
+bool RmScan::is_end() const {
+    // Todo: 修改返回值
+    if(rid_.page_no==-1&&rid_.slot_no==-1)
+        return true;
+    return false;
+}
+
+/**
+ * @brief RmScan内部存放的rid
+ */
+Rid RmScan::rid() const {
+    // Todo: 修改返回值
+    return rid_;
+}
